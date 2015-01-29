f0b7e956eb5a0cb78cce0b062b234d47954f5704
FlyInTheDarkNess <916879006@qq.com>
2015-01-29 22:40:11 +0800

    Initial Commit

---
+++ UnKnowSoftware/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UnKnowSoftware
+//
+//  Created by 赵中良 on 15/1/29.
+//  Copyright (c) 2015年 com.iuiue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UnKnowSoftware/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UnKnowSoftware
+//
+//  Created by 赵中良 on 15/1/29.
+//  Copyright (c) 2015年 com.iuiue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
