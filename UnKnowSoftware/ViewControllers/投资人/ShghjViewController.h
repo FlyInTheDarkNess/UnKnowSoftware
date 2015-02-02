85672c032bb90de9409664eefc35371b125cf254
FlyInTheDarkNess <916879006@qq.com>
2015-02-02 20:22:22 +0800

    guguguguyguy

---
+++ UnKnowSoftware/ViewControllers/投资人/ShghjViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShghjViewController.h
+//  UnKnowSoftware
+//
+//  Created by 小小程序员 on 15/2/2.
+//  Copyright (c) 2015年 com.iuiue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShghjViewController : UIViewController
+
+@end
---
+++ UnKnowSoftware/ViewControllers/投资人/ShghjViewController.m
@@ -0,0 +1,37 @@
+//
+//  ShghjViewController.m
+//  UnKnowSoftware
+//
+//  Created by 小小程序员 on 15/2/2.
+//  Copyright (c) 2015年 com.iuiue. All rights reserved.
+//
+
+#import "ShghjViewController.h"
+
+@interface ShghjViewController ()
+
+@end
+
+@implementation ShghjViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
